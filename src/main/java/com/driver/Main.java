0bf4bf63a65ffe8a802b4265044abffbb97c1a80
Arjun chand <chandarjun10@gmail.com>
2023-09-17 16:11:12 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Arjun";
+        //name has private access in com.driver.RWOnly
+        obj.setName("Arjun");
+        System.out.println(obj.getName());
+    }
   
 }
